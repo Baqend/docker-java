66a625de3963bd0301c5454113c86e7e68efdadd
fb <fb@baqend.com>
2017-02-21 00:06:47 +0100

    automate java build

--- update.sh
+++ update.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-sed "s/ARG JRE_MAJOR/ARG JRE_MAJOR=$JRE_MAJOR/g; s/ARG JRE_UPDATE/ARG JRE_UPDATE=$JRE_UPDATE/g; s/ARG JRE_BUILD/ARG JRE_BUILD=$JRE_BUILD/g" Dockerfile.template > node/Dockerfile
-
-TAG="${JRE_MAJOR}u${JRE_UPDATE}"
+sed "s@ARG JDK_URL@ARG JDK_URL=$JDK_URL@g" Dockerfile.template > java/Dockerfile
 
 git add java/Dockerfile
 git commit -m "New java version ${JRE_MAJOR}u${JRE_UPDATE}"
-git tag -f $TAG
+git tag -f $JDK_TAG
 git push origin master
-git push -f origin $TAG
+git push -f origin $JDK_TAG
