bec26db78748871399d04e8fe2f2c76748495307
fb <fb@baqend.com>
2017-02-21 10:49:24 +0100

    fix commit message

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 sed "s@ARG JDK_URL@ARG JDK_URL=$JDK_URL@g" Dockerfile.template > java/Dockerfile
 
 git add java/Dockerfile
-git commit -m "New java version ${JRE_MAJOR}u${JRE_UPDATE}"
+git commit -m "New java version $JDK_TAG"
 git tag -f $JDK_TAG
 git push origin master
 git push -f origin $JDK_TAG
